bc9776f57e1f463b41b37c2ae3f335b6b97eea96
naumov78 <naumov78@gmail.com>
2016-11-16 18:01:35 -0500

    Add files via upload

---
+++ HWRK_W5D3.js
@@ -0,0 +1,72 @@
+
+function madLib(verb, adj, noun) {
+  return "We shall `${verb.toUpperCase()}` the `${adj.toUpperCase()}` `${noun.toUpperCase()}`.";
+}
+
+
+function isSubstring(phrase, subphrase) {
+  return phrase.includes(subphrase);
+}
+
+function fizzBuzz (array) {
+  const fizzBuzzArr = [];
+
+  array.forEach(el => {
+    if ((el % 3 === 0) ^ (el % 5 === 0)) {
+      fizzBuzzArr.push(el);
+    }
+  });
+
+  return fizzBuzzArr;
+}
+
+
+function isPrime (n) {
+  if (n < 2) { return false; }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+function sumOfNPrimes (n) {
+  let sum = 0;
+  let countPrimes = 0;
+  let i = 2;
+
+  while (countPrimes < n) {
+    if (isPrime(i)) {
+      sum += i;
+      countPrimes++;
+    }
+    i++;
+  }
+
+  return sum;
+}
+
+
+// function allOrNothing(mod, numbers) {
+//
+// }
+
+
+function titleize(array, callback) {
+  let result = array.map(function(el){
+    "Mx. `${el}` Jingleheimer Schmidt";
+  }
+);
+  result.forEach(function(el){
+    callback(el);
+  }
+);
+}
+
+var eee = titleize(["Joe", "Bob"], function(el){
+  console.log(el)
+});
